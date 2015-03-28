07877bb5d217e49fd1babc9d931dee6ad76a6fb8
Mark Cooper <markchristophercooper@gmail.com>
2015-03-28 10:46:19 -0700

    Cleanup arg and bootstrap handling

--- bin/cscli-import.js
+++ bin/cscli-import.js
@@ -22,9 +22,25 @@ if (require.main === module) {
 
   var importer = new importer(api, argv).getImporter(type);
 
-  if (argv.help || !exists || !importer.validate()) {
+  if(!exists) {
+    console.log("Input directory does not exist");
+    process.exit(1);
+  }
+
+  if(typeof importer === "undefined") {
+    console.log("Invalid importer type specified");
+    process.exit(1);
+  }
+
+  if(argv['help']) {
     console.log(importer.help());
     process.exit(0);
+  }
+
+  if(!importer.valid()) {
+    console.log("Importer options are invalid");
+    console.log(importer.help());
+    process.exit(1);
   }
 
   // loop dir, read files, send to importer
