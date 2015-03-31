64025784bece205b379528d0c6b4f844a8fe752c
Mark Cooper <markchristophercooper@gmail.com>
2015-03-31 15:43:36 -0700

    Outline for batch deleting records by path

--- bin/cscli-nuke.js
+++ bin/cscli-nuke.js
@@ -1,7 +1,44 @@
 #!/usr/bin/env node
 
+var fs    = require('fs');
+var queue = require('queue-async');
+var util  = require('util');
+
+var config = require('../lib/config');
+var nuke   = require('../lib/nuke');
+
+var csapi = require('csapi');
+var csapi = new csapi(config);
+
 exports.command = {
   description: "Nuke records in collectionspace!"
 }
 
 // prompt to confirm
+if (require.main === module) {
+  var argv   = require('minimist')(process.argv.slice(2));
+  var q      = queue(1);
+  var table  = require('cli-table');
+
+  var file   = argv['file'];
+  var exists = fs.existsSync(file);
+
+  if(argv['help'] || !exists) {
+    console.log("cscli nuke --file=<file>");
+    process.exit(1);
+  }
+
+  // read file to get paths
+  var opts = {
+    api: csapi,
+    paths: ['/path/A', '/path/B', '/path/C'],
+    queue: q
+  };
+
+  var nuke = new nuke(opts).fire();
+  q.awaitAll(function(error, results) {
+    results.forEach(function(result) {
+      console.log("Deleted: " + result);
+    });
+  });
+}
