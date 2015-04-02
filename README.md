a892b77a93435b31ece1f6f318496d34f72e4a10
Mark Cooper <markchristophercooper@gmail.com>
2015-04-01 21:48:59 -0700

    Add list script and update docs

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 CollectionSpace CLI
 ===================
 
-Requires the `csapi` client.
+Requires the [csapi](https://github.com/mark-cooper/csapi) client.
 
 Quickstart
 ----------
@@ -11,6 +11,8 @@ Quickstart
 npm install mark-cooper/cscli -g
 cscli setup
 ```
+
+This will create a `.cscli` file under `~/` to store backend credentials.
 
 **Import**
 
@@ -27,5 +29,23 @@ cscli import --type=person --shortid=person --dir=examples/person/person
 # create cataloging records (may reference authority records)
 cscli import --type=collectionobject --dir=examples/collectionobjects
 ```
+
+**List**
+
+List records by path:
+
+```bash
+cscli list --path=media --properties=title,uri
+
+cscli list --path=personauthorities --properties=shortIdentifier,csid
+# use csid from personauthorities to get person records
+cscli list --path=personauthorities/92c6d196-d88e-4e0e-8dbb/items --properties=shortIdentifier,csid
+```
+
+Use `--format=csv` if required, otherwise `table` is the default.
+
+**Nuke**
+
+This will delete records from CollectionSpace referenced by `path` from a file of `uri` values. Use with caution!
 
 ---
---
+++ bin/cscli-list.js
@@ -0,0 +1,49 @@
+#!/usr/bin/env node
+
+var util   = require('util');
+var config = require('../lib/config');
+var csapi  = require('csapi');
+var csapi  = new csapi(config);
+
+exports.command = {
+  description: "list records from collectionspace"
+}
+
+if (require.main === module) {
+  var argv       = require('minimist')(process.argv.slice(2));
+  var table      = require('cli-table');
+  var format     = argv['format'] || 'table';
+  var path       = argv['path'];
+  var properties = argv['properties'] || 'uri';
+  var size       = argv['size'] || 100;
+  var tabular    = format === "table" ? true : false;
+
+  if(argv['help'] || util.isNullOrUndefined(path)) {
+    console.log("cscli list --path=<path> [--format=<table> --properties=<properties|uri> --size=<size|100>]");
+    process.exit(1);
+  }
+
+  var properties = properties.split(",");
+  var output = tabular ? new table({ head: properties }) : [];
+
+  csapi.getListByPath('/' + path, { pgSz: 100, wf_deleted: false }, function(err, res, data) {
+    // process data
+    data['list-item'].forEach(function(item){
+      var values = [];
+      properties.forEach(function(property) {
+        value = item.hasOwnProperty(property) ? item[property][0] : '';
+        values.push(value);
+      });
+      output.push(values);
+    });
+
+    // process output
+    if(tabular) {
+      console.log(output.toString());
+    } else {
+      output.forEach(function(data) {
+        console.log(data.join());
+      });
+    }
+  });
+}
